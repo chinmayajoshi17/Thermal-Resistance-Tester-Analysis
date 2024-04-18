aa8788295459dd7400d5ac081044f482e9c3353b
chinmayajoshi17 <165985337+chinmayajoshi17@users.noreply.github.com>
2024-04-17 19:45:38 -0500

    Additional Data Reporting + Temperature Plot Over Setup
    Reports temperature gradients over hot and cold sides, and sample. New plot added to show temperature profile over the entire test setup.

--- Joshi_TRT_MATLABCode.m
+++ Joshi_TRT_MATLABCode.m
@@ -1,11 +1,27 @@
+%{
+Author: Chinmaya Joshi
+Organisation: NED3 - University of Arkansas
+ASTM D5470 Thermal Resistance Tester
+Steady-State Analysis
+distance between each point for distances = 1E-06
+%}
 
-TRT = load('Test.txt');
+%Use 'clear' and 'clc' commands after every run
+
+%Input Test Data Filename below:
+TestDataFilename = 'TRT-004'; %Test data filenames are saved as TRT-00X
 %Input Sample Thickness below:
-ySample = 0.0000; %Thickness of sample in metres
+ySample = 0.00126; %Thickness of sample in m
+ySampleLine = linspace(0,ySample, ySample * 10^6);
 
+%Analysis Code Starts:
+TestNumber = TestDataFilename(1,(5:7));
+FileExtension = '.txt';
+DataInput = append(TestDataFilename,FileExtension);
+
+TRT = load(DataInput); 
 %pulling data from MATLAB workspace file
 Data = TRT;
-
 t = Data(:,1); %Time of txperiment in s with 1000Hz sample rate
 
 %Cold side temperatures
@@ -43,7 +59,7 @@ yThermocouple = 0.0136;
 ySpacing = [0.0044, 0.0180, 0.0316];
 n = 3; %number of thermocouplees/datapoints on each side, required for least squares regression
 
-areaSample = 0.016*0.016; %Area os sample in m^2
+areaSample = 0.016*0.016; %Area of sample in m^2
 kAl = 152; %6061 Aluminium Thermal Conductivity
 
 %Calculating temperature gradients between thermocouples
@@ -86,24 +102,39 @@ Hintercept = (sumHT - Hslope*sumy)/n;
 Cintercept = (sumCT - Cslope*sumy)/n;
 
 %Using slopes and intercepts from LSR to extrapolate temps on test surfaces
-length = linspace(0,0.0360,10000);
+length = linspace(0,0.0360,36000); %length of 1 aluminium block
 HTL = Hslope.*length + Hintercept;
 CTL = Cslope.*length + Cintercept;
 
+reversedHTL = fliplr(HTL);
 
-HTLsize = size(HTL,2);
-CTLsize = size(CTL,2);
+slopeSample = (HTL(1,36000) - CTL(1,36000))/ySample;
+sampleTemps = slopeSample.*ySampleLine + CTL(1,36000);
+
+totalLength = (2 * 0.0360) + ySample;
+experimentalLength = linspace(0,0.073260,73260);
+
+HTLsize = size(HTL,2); %
+CTLsize = size(CTL,2); %
 
 Thot = HTL(1,HTLsize);
 Tcold = CTL(1,CTLsize);
+
+temperatures = [CTL sampleTemps reversedHTL];
+
 
 %Final calculations
 TgradSample = (Thot-Tcold)/ySample; %Temperature gradient over the sample in K/m
 kGraphite = AvgFlux/TgradSample; %Thermal conductivity of graphite sample in W/m*K
 rGraphite = ySample/(kGraphite*areaSample); %Thermal resistance of graphite sample in K/W
+rGraphiteAreaCompensated = rGraphite*areaSample; %Thermal resistance of graphite sample in K*m^2/W
 
-%Plotting data
 
+%Analysis Code Ends
+
+%Plotting Data:
+figure(1);
+%Plotting Temperatures vs Time
 plot(t,sH1);
 hold on
 plot(t,sH2);
@@ -116,15 +147,64 @@ plot(t,sC2);
 hold on
 plot(t,sC3);
 hold on
+xline(600,'--b',{'Steady', 'State'});
+yticks(linspace(10,120,12));
+ylim([10 120]);
+grid on;
+title(TestDataFilename,'Thermocouple Temperatures vs Time')
+xlabel('Time (s)');
+ylabel('Temperature (^{o}C)');
 
-
-%LSR lines
-%{
+%Interpolation Plot
+figure(2);
 plot(length,HTL);
 hold on
+plot(length,HTL,'-x','MarkerIndices', 36000);
+hold on
+plot(length,HTL,'-s','MarkerIndices', [36000*0.122, 36000*0.5, 36000*0.877]);
+hold on
 plot(length,CTL);
+hold on
+plot(length,CTL,'-x','MarkerIndices', 36000);
+hold on
+plot(length,CTL,'-s','MarkerIndices', [36000*0.122, 36000*0.5, 36000*0.877]);
+hold on
+xline(0.036,'--b',{'Graphite', 'Surface', 'Temperature'})
+yticks(linspace(10,120,12));
+ylim([10 120]);
+grid on;
+title(TestDataFilename,'Interpolating Surface Temperature on Graphite Sample')
+xlabel('Location on Al Block(m)');
+ylabel('Temperature (^{o}C)');
 
-HTLsize = size(HTL,2)
-CTLsize = size(CTL,2)
 
-%}
+%Temperatures Plot
+
+figure(3);
+plot(experimentalLength,temperatures);
+hold on
+plot(experimentalLength,temperatures,'-x','MarkerIndices', 36000);
+hold on
+plot(experimentalLength,temperatures,'-x','MarkerIndices', 37260);
+hold on
+plot(experimentalLength, temperatures,'-s','MarkerIndices', [4392, 18000, 31572]);
+hold on
+plot(experimentalLength, temperatures,'-s','MarkerIndices', [41652, 55260, 68832]);
+hold on
+xline(0.036000,'--b'); %{'Graphite Cold Surface Temperature'}
+hold on
+xline(0.037260,'--b'); %{'Graphite Hot Surface Temperature'}
+yticks(linspace(10,120,12));
+ylim([10 120]);
+xlim([0 0.073260]);
+grid on;
+title(TestDataFilename,'Temperatures Across Test Setup');
+xlabel('Location on Tester (m)');
+ylabel('Temperature (^{o}C)');
+
+%Reporting Data
+disp("Temperature Gradient over Hot Side is " + abs(Hslope) + " K/m");
+disp("Temperature Gradient over Cold Side is " + Cslope + " K/m");
+disp("Temperature Gradient over Sample is " + slopeSample + " K/m");
+disp("Thermal Resistance of the Graphite Sample is " + rGraphiteAreaCompensated + " m^2 K/W");
+
